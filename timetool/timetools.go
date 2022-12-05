f61edf26f81f331328b5ab9f0a73c7b49966d7d9
夏毓胤 <xiayuyin@newrank.cn>
2022-12-05 13:03:04 +0800

    init

--- timetool/timetools.go
+++ timetool/timetools.go
@@ -1,8 +1,14 @@
 package timetool
 
+import "fmt"
+
 type TimeTools struct {
 }
 
 func NewTimeTools() *TimeTools {
 	return &TimeTools{}
+}
+
+func (t *TimeTools) Hello() {
+	fmt.Println("hello world!")
 }
