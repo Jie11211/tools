cfcb610625457e4a9ce6c2b158e25b15d3b3d09c
夏毓胤 <xiayuyin@newrank.cn>
2022-12-05 14:36:02 +0800

    timetool

--- timetool/timetools.go
+++ timetool/timetools.go
@@ -1,6 +1,12 @@
 package timetool
 
-import "fmt"
+import (
+	"time"
+)
+
+const(
+	layout="2006-01-02 15:04:05"
+)
 
 type TimeTools struct {
 }
@@ -9,6 +15,61 @@ func NewTimeTools() *TimeTools {
 	return &TimeTools{}
 }
 
-func (t *TimeTools) Hello() {
-	fmt.Println("hello world!")
+func (t *TimeTools)SleepToNextMinute(){
+	now := time.Now()
+	next := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), 0, 0, now.Location()).Add(time.Minute)
+	time.Sleep(time.Duration(int(next.Unix()-now.Unix())) * time.Second)
+}
+
+func (t *TimeTools)SleepMinute(minute int){
+	now := time.Now()
+	next := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), 0, 0, now.Location()).Add(time.Minute*time.Duration(minute))
+	time.Sleep(time.Duration(int(next.Unix()-now.Unix())) * time.Second)
+}
+
+func (t *TimeTools)SleepToNextHour(){
+	now := time.Now()
+	next := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), 0, 0, 0, now.Location()).Add(time.Hour)
+	time.Sleep(time.Duration(int(next.Unix()-now.Unix())) * time.Second)
+}
+
+func (t *TimeTools)SleepHour(hour int){
+	now := time.Now()
+	next := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), 0, 0, 0, now.Location()).Add(time.Hour*time.Duration(hour))
+	time.Sleep(time.Duration(int(next.Unix()-now.Unix())) * time.Second)
+}
+
+func (t *TimeTools)SleepToNextDay(){
+	now := time.Now()
+	next := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).Add(time.Hour*24)
+	time.Sleep(time.Duration(int(next.Unix()-now.Unix())) * time.Second)
+}
+
+func (t *TimeTools)SleepDay(day int){
+	now := time.Now()
+	next := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).Add(time.Hour*24*time.Duration(day))
+	time.Sleep(time.Duration(int(next.Unix()-now.Unix())) * time.Second)
+}
+
+func (t *TimeTools)SleepParse(timeStr string)error{
+	now := time.Now()
+	next, err := t.ParseInLocation(timeStr)
+	if err!=nil{
+		return err
+	}
+	time.Sleep(time.Duration(int(next.Unix()-now.Unix())) * time.Second)
+	return nil
+}
+
+func (t *TimeTools)ParseInLocation(timeStr string)(time.Time,error){
+	next, err := time.ParseInLocation(layout, timeStr, time.Local)
+	return next,err
+}
+
+func  (t *TimeTools)TimeParseToString(in time.Time)string{
+	return in.Format(layout)
+}
+
+func  (t *TimeTools)TimeParseToTime(in time.Time)(time.Time,error){
+	return t.ParseInLocation(in.Format(layout))
 }
